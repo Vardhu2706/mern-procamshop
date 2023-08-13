cd9356aaca02c27736f43d6393674c45af82f56b
Vardhu2706 <ayyagari.vardhu@gmail.com>
2023-08-13 15:34:48 +0530

    Added PlaceOrder screen

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,12 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
 const port = process.env.PORT || 5000;
+
+// Routes
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+
 import colors from "colors";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import cookieParser from "cookie-parser";
@@ -25,6 +29,8 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+
 app.use(notFound);
 app.use(errorHandler);
 
